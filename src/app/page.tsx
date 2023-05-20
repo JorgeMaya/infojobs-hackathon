6c7083864fd6b9efd6d7118a47afd81d625b047c
Fernando Maya <ferchomaya29@gmail.com>
2023-05-20 15:29:45 -0600

    import github component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from "next/image";
 import AccessToken from "./api/infoJobs";
+import { GitHubComponent } from "./components/gitHubComponent";
 const { getAccessToken } = AccessToken();
 const handleClick = async () => {
   const auth2 = await getAccessToken();
