41755386d392c4b76f944715ad966b0dbc1952fe
JorgeMaya <jorgitomaya1@gmail.com>
2023-05-27 23:06:09 -0600

    merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,11 @@
-import { Card, Title, Text, Grid, Col } from "@tremor/react";
-import { DetailsComponent } from "./components/detailsComponent";
-import { GitHubComponent } from "./components/gitHubComponent";
-import { getReposByUser, getUser, getUserLanguages } from "./services/githubService";
-import { DesirePositionComponent } from "./components/desirePositionComponent";
-import { SkillComponent } from "./components/skillComponent";
+import { AppContextProvider } from "./context/app-context";
+import { HomePage} from "./pages/homePage";
 
 export default async function Home() {
-	const gitUserDetails = await getUser("midudev");
-	const gitReposByUser = await getReposByUser("midudev");
-	const gitLanguages = await getUserLanguages("midudev");
 
 	return (
-		<main className="w-full border-none h-full p-4 sm:p-10 border-x border-gray-200">
-			<Grid numColsLg={6} className="gap-6">
-				<Col numColSpanLg={4}>
-					<Card className="h-full">
-						<DetailsComponent />
-					</Card>
-				</Col>
-
-				<Col numColSpanLg={2}>
-					<div className="space-y-6">
-						<Card
-							decoration="left"
-							decorationColor="cyan"
-							key={gitUserDetails.login}
-							className="h-fit"
-						>
-							<DesirePositionComponent />
-						</Card>
-						<Card
-							decoration="left"
-							decorationColor="purple"
-							key={gitUserDetails.login}
-							className="h-fit"
-						>
-							<GitHubComponent
-								gitUserDetails={gitUserDetails}
-								gitRepos={gitReposByUser}
-								gitUserLanguages={gitLanguages}
-							/>
-						</Card>
-					</div>
-				</Col>
-			</Grid>
-		</main>
+      <AppContextProvider>
+        <HomePage />
+      </AppContextProvider>
 	);
 }
---
+++ src/app/pages/homePage.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { Card, Grid, Col } from "@tremor/react";
+import { DetailsComponent } from "../components/detailsComponent";
+import {
+	getReposByUser,
+	getUser,
+	getUserLanguages,
+} from "../services/githubService";
+import { DesirePositionComponent } from "../components/desirePositionComponent";
+import { GitHubComponent } from "../components/gitHubComponent";
+import { useContext, useEffect, useState } from "react";
+import AppContext from "../context/app-context";
+import HeaderComponent from "../components/headerComponent";
+import { GitUserDetails } from "../types";
+import { GitRepo } from "../types";
+
+export function HomePage(props: {}) {
+	const [gitUserDetails, setGitUserDetails] = useState({} as GitUserDetails);
+	const [gitReposByUser, setGitReposByUser] = useState([] as GitRepo[]);
+	const [gitLanguages, setGitLanguages] = useState([] as string[]);
+
+	const getGitData = async () => {
+		setGitUserDetails(await getUser("midudev"));
+		setGitReposByUser(await getReposByUser("midudev"));
+		setGitLanguages(await getUserLanguages("midudev"));
+	};
+	const { setState } = useContext(AppContext);
+
+	useEffect(() => {
+		const url = window.location.href;
+		const code = url.split(/[?=&]/)[2];
+		//console.log("code", code);
+		setState(code);
+		getGitData();
+	}, []);
+
+	return (
+		<>
+			<HeaderComponent />
+			<main className="w-full border-none h-full p-4 sm:p-10 border-x border-gray-200">
+				<Grid numColsLg={6} className="gap-6">
+					<Col numColSpanLg={4}>
+						<Card className="h-full">
+							<DetailsComponent />
+						</Card>
+					</Col>
+
+					<Col numColSpanLg={2}>
+						<div className="space-y-6">
+							<Card
+								decoration="left"
+								decorationColor="cyan"
+								key={gitUserDetails.login}
+								className="h-fit"
+							>
+								<DesirePositionComponent />
+							</Card>
+							<Card
+								decoration="left"
+								decorationColor="purple"
+								key={gitUserDetails.login}
+								className="h-fit"
+							>
+								<GitHubComponent
+									gitUserDetails={gitUserDetails}
+									gitRepos={gitReposByUser}
+									gitUserLanguages={gitLanguages}
+								/>
+							</Card>
+						</div>
+					</Col>
+				</Grid>
+			</main>
+		</>
+	);
+}
