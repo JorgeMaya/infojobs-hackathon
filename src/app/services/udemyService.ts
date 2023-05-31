ef8ad4eebae44212b9325054b1164a8015d4c3d3
Fernando Maya <ferchomaya29@gmail.com>
2023-05-30 23:25:22 -0600

    UDEMY api

--- src/app/services/udemyService.ts
+++ src/app/services/udemyService.ts
@@ -1,8 +1,10 @@
+import config from "../config";
+
 export async function getCourses() {
 	const res = await fetch(`https://www.udemy.com/api-2.0/courses/?page=1&page_size=5`, {
 		headers: {
 			"Content-Type": "application/json",
-            "Authorization": "Basic V2pFQlVJUlNkb0V2UVlxRFV5TmFRMDl5eXo2cnN6ZVlPZ01SVkdCMDpKaTRVV2JHSXVhTkFsZFZjTnBNSFdZRm5hdFlMaTBUOU8zSVRRNHRaR1BjMExVNzNHcWJGa1dXZU1TemJtVGlkT3dRNEtDMW5XSEhxaWtOaG5KdHZ5SmpCNTF3blZTNkFCeFZzUkZoS012Q0R2T0IzR05aU2FmVHFGNmFERFQzWg=="
+            "Authorization": `Basic ${config.udemyToken}`
 		},
 	});
 
@@ -10,3 +12,29 @@ export async function getCourses() {
     console.log(coursesDetails);
 	return coursesDetails;
 }
+
+export async function getCourseById(id:any) {
+	const res = await fetch(`https://www.udemy.com/api-2.0/courses/${id}/`, {
+		headers: {
+			"Content-Type": "application/json",
+            "Authorization": `Basic ${config.udemyToken}`
+		},
+	});
+
+	const courseDetails = await res.json();
+    console.log(courseDetails);
+	return courseDetails;
+}
+
+// export async function getCourses() {
+// 	const res = await fetch(`https://www.udemy.com/api-2.0/courses/?page=1&page_size=5`, {
+// 		headers: {
+// 			"Content-Type": "application/json",
+//             "Authorization": `Basic ${config.clientSecretUdemy}`
+// 		},
+// 	});
+
+// 	const coursesDetails = await res.json();
+//     console.log(coursesDetails);
+// 	return coursesDetails;
+// }
